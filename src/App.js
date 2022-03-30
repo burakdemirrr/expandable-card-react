f6ff22a94d9e461001a98e425f64143b4b63bd6d
Burak <mburak.demir.059@gmail.com>
2022-03-30 14:43:46 +0300

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { motion } from "framer-motion";
+import { useState } from 'react';
 function App() {
+
+  const [open, setOpen] = useState(false);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <motion.div transition={{layout:{duration:1,type:"spring"}}} style={{borderRadius:'1rem',boxShadow:'0px 10px 30px rgba(0,0,0,0.5)'}} layout onClick={() => setOpen(!open)} className='card'>
+        <motion.h2 layout="position">Framer Motion</motion.h2>
+        {open && (<motion.div className='expand' initial={{opacity:0}}
+        animate={{opacity:1}} transition={{duration:1}}>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni maxime molestias explicabo similique iste harum voluptatum perspiciatis asperiores aut. Eos.</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, temporibus.</p>
+          <button>Hello</button>
+        </motion.div>
+        )
+        }
+      </motion.div>
     </div>
   );
 }
